34b67d87fc4a941b7f304ab2a8009bcb4168489e
Tiago Angelo <kurtis.angelo@gmail.com>
2024-12-01 12:26:08 -0300

    Fourth commit

--- cmd/app/wire_provs.go
+++ cmd/app/wire_provs.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"github.com/google/wire"
+
+	"github.com/angelokurtis/go-home-automations/pkg/app"
 )
 
-var Providers = wire.NewSet()
+var Providers = wire.NewSet(
+	wire.Bind(new(AppRunner), new(*app.Runner)),
+	app.Providers,
+)
--- pkg/app/runner.go
+++ pkg/app/runner.go
@@ -1,3 +1,16 @@
 package app
 
+import (
+	"context"
+)
+
 type Runner struct{}
+
+func NewRunner() *Runner {
+	return &Runner{}
+}
+
+func (r *Runner) Run(ctx context.Context) error {
+	// TODO implement me
+	panic("implement me")
+}
