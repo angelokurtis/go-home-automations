d49ba15d168a353ab18fb916569ddc36538a8039
Tiago Angelo <kurtis.angelo@gmail.com>
2025-03-19 21:35:32 -0300

    Forty-first commit

---
+++ pkg/entity/synchronizer_listener.go
@@ -0,0 +1,57 @@
+package entity
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/samber/lo"
+	"github.com/sourcegraph/conc/pool"
+	ga "saml.dev/gome-assistant"
+
+	"github.com/angelokurtis/go-home-automations/internal/errors"
+)
+
+type SynchronizedSwitchesListener struct {
+	service   *ga.Service
+	entityIds []string
+}
+
+func NewSynchronizedSwitchesListener(service *ga.Service, entityIds ...string) *SynchronizedSwitchesListener {
+	return &SynchronizedSwitchesListener{service: service, entityIds: entityIds}
+}
+
+func (l *SynchronizedSwitchesListener) OnChange(ctx context.Context, entity ga.EntityData) error {
+	entityIds := lo.Without(l.entityIds, entity.TriggerEntityId)
+	p := pool.New().WithMaxGoroutines(10).WithErrors()
+
+	for _, entityId := range entityIds {
+		entityId := entityId // avoid loop variable capture
+
+		p.Go(func() error {
+			if entity.ToState == "on" {
+				if err := l.service.HomeAssistant.TurnOn(entityId); err != nil {
+					return errors.WithStack(err)
+				}
+			}
+
+			if entity.ToState == "off" {
+				if err := l.service.HomeAssistant.TurnOff(entityId); err != nil {
+					return errors.WithStack(err)
+				}
+			}
+
+			slog.InfoContext(ctx, "Entity state updated successfully",
+				slog.String("entity-id", entityId),
+				slog.String("new-state", entity.ToState),
+			)
+
+			return nil
+		})
+	}
+
+	return p.Wait()
+}
+
+func (l *SynchronizedSwitchesListener) EntityIds() []string {
+	return l.entityIds
+}
