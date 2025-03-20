6761ca85770d03a166ec4cc8bbfc6bc1d12416ff
Tiago Angelo <kurtis.angelo@gmail.com>
2025-03-19 22:10:41 -0300

    Fifty-first commit

--- cmd/app/wire_gen.go
+++ cmd/app/wire_gen.go
@@ -12,10 +12,6 @@ import (
 	"github.com/angelokurtis/go-home-automations/pkg/app"
 )
 
-import (
-	_ "github.com/angelokurtis/go-home-automations/internal/logger"
-)
-
 // Injectors from wire_inj.go:
 
 func NewRunner(ctx context.Context) (Runner, func(), error) {
